9c1e42e32298d57ec2029c8cbae06b37ffa71e37
kinana52 <93495806+kinana52@users.noreply.github.com>
2022-02-05 00:34:32 -0800

    first commit

---
+++ client/src/components/Navbar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+<div id="navbar">
+            <div className="title">
+                <h1>Pokemon Generator</h1>
+            </div>
+            <div className="nav-buttons">
+                <a href="/create">Create New Pokemon</a>
+                <a href="/view">View All Pokemon</a>
+            </div>
+        </div>
